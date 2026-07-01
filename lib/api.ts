a531fae7fa351fd8d56cb89e2fad5074f792aea6
Slavik <slavik1058@gmail.com>
2026-07-02 01:30:58 +0200

    add all components and their styles

---
+++ lib/api.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import type { Note } from "../types/note";
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+
+const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN
+
+export interface NotesHttpResponse{
+    notes: Note[];
+    page?: number;
+    totalPages?: number;
+}
+
+export const fetchNotes = async (page: number, searchText: string) => {
+    const res = await axios.get<NotesHttpResponse>("/notes", {
+        params: {
+            page: page,
+            perPage: 12,
+            search: searchText
+        },
+        headers:{
+            Authorization: `Bearer ${myToken}`
+        }
+    });
+    return res.data;
+};
+
+interface NewNote{
+    title: string;
+    content: string;
+    tag: string
+}
+
+export const createNote = async(newNote: NewNote) =>{
+    const res = await axios.post<Note>("/notes", newNote, {
+        headers:{
+            Authorization: `Bearer ${myToken}`
+        }
+    });
+    return res.data;
+}
+
+export const deleteNote = async(noteId: string)=>{
+    const res = await axios.delete<Note>(`/notes/${noteId}`, {
+        headers:{
+            Authorization: `Bearer ${myToken}`
+        }
+    });
+    return res.data
+}
+
+
